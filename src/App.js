cabdc0c88c2251d212cc3778d9f82086a2008f41
od-frontender <od.frontender@gmail.com>
2024-01-03 01:03:21 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Table from "./components/Table/Table";
+import Heading from "./components/Headind/Heading";
+
+function App() {
+  return (
+    <>
+      <Heading />
+      <Table />
+    </>
+  );
+}
+export default App;
